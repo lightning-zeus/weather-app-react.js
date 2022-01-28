79f67e55f7e4f9055fb77227b550382906c2189b
Tuhin Mondal <tuhinchess@gmail.com>
2022-01-28 22:34:26 +0530

    Search Bar entry extracted

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,24 +1,57 @@
 import React, { Component } from "react";
-function SearchBar() {
-  return (
-    <div
-      className="input"
-      style={{
-        display: "flex",
-        flexDirection: "",
-        textAlign: "center",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <input
-        type="text"
-        style={{ height: "40px", fontWeight: "bold", borderRadius: "10px" }}
-        className="inputValue"
-        placeholder="Enter a City"
-      ></input>
-      <input type="submit" value="Submit"></input>
-    </div>
-  );
+class SearchBar extends Component {
+  state = {
+    city: "",
+    url: "",
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log(prevState, this.state);
+  }
+
+  handleEnter = (event) => {
+    event.preventDefault();
+    this.setState({
+      city: this.inputNode.value,
+      url: `http://api.openweathermap.org/data/2.5/weather?q= 
+        ${this.state.city} 
+        &units=metric&appid=cb4d3020367da2edfedc7ab07356eb3f`,
+    });
+    console.log(this.state.city);
+  };
+
+  render() {
+    return (
+      <div
+        className="input"
+        style={{
+          display: "flex",
+          flexDirection: "",
+          textAlign: "center",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <form>
+          <input
+            type="text"
+            style={{ height: "40px", fontWeight: "bold", borderRadius: "10px" }}
+            className="inputValue"
+            name="cityname"
+            ref={(node) => (this.inputNode = node)}
+            placeholder="Enter a City"
+          ></input>
+          <button
+            onClick={this.handleEnter}
+            className="btn btn-secondary btn-sm"
+          >
+            Enter
+          </button>
+        </form>
+        <span style={{ color: "white" }}>{this.state.city}</span>
+      </div>
+    );
+  }
 }
+
 export default SearchBar;
